42decc1cdadb1e490f9a692455b51af818608019
utsho-anik <114429458+utsho-anik@users.noreply.github.com>
2023-06-01 13:21:02 +0600

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,292 +1,58 @@
- INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (1, 'Fiction', 3, 2);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (2, 'Science', 5, 1);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (3, 'Biography', 2, 4);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (4, 'History', 8, 3);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (5, 'Poetry', 1, 5);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (6, 'Mystery', 6, 2);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (7, 'Art', 10, 1);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (8, 'Travel', 4, 4);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (9, 'Self-help', 9, 3);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (10, 'Drama', 7, 5);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (11, 'Fantasy', 2, 1);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (12, 'Romance', 6, 3);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (13, 'Science Fiction', 9, 2);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (14, 'Business', 4, 1);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (15, 'Thriller', 8, 4);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (16, 'Cooking', 1, 2);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (17, 'Health', 5, 3);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (18, 'Sports', 10, 1);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (19, 'Music', 3, 4);
-
-INSERT INTO Library (category_id, category, self_no, row_no)
-VALUES (20, 'Religion', 7, 5);
-
-
-
-
-
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (1, 1, 'Book 1', 'Publisher A', 'First Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (2, 2, 'Book 2', 'Publisher B', 'Second Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (3, 3, 'Book 3', 'Publisher C', 'Third Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (4, 4, 'Book 4', 'Publisher D', 'Fourth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (5, 5, 'Book 5', 'Publisher E', 'Fifth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (6, 6, 'Book 6', 'Publisher F', 'Sixth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (7, 7, 'Book 7', 'Publisher G', 'Seventh Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (8, 8, 'Book 8', 'Publisher H', 'Eighth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (9, 9, 'Book 9', 'Publisher I', 'Ninth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (10, 10, 'Book 10', 'Publisher J', 'Tenth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (11, 1, 'Book 11', 'Publisher A', 'First Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (12, 2, 'Book 12', 'Publisher B', 'Second Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (13, 3, 'Book 13', 'Publisher C', 'Third Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (14, 4, 'Book 14', 'Publisher D', 'Fourth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (15, 5, 'Book 15', 'Publisher E', 'Fifth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (16, 6, 'Book 16', 'Publisher F', 'Sixth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (17, 7, 'Book 17', 'Publisher G', 'Seventh Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (18, 8, 'Book 18', 'Publisher H', 'Eighth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (19, 9, 'Book 19', 'Publisher I', 'Ninth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (20, 10, 'Book 20', 'Publisher J', 'Tenth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (21, 1, 'Book 21', 'Publisher 1', 'First Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (22, 2, 'Book 22', 'Publisher 2', 'Second Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (23, 3, 'Book 23', 'Publisher 3', 'Third Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (24, 4, 'Book 24', 'Publisher 4', 'Fourth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (25, 5, 'Book 25', 'Publisher 5', 'Fifth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (26, 6, 'Book 26', 'Publisher 6', 'Sixth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (27, 7, 'Book 27', 'Publisher 7', 'Seventh Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (28, 8, 'Book 28', 'Publisher 8', 'Eighth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (29, 9, 'Book 29', 'Publisher 9', 'Ninth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (30, 10, 'Book 30', 'Publisher 10', 'Tenth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (31, 11, 'Book 31', 'Publisher 11', 'First Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (32, 13, 'Book 32', 'Publisher 13', 'Second Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (33, 15, 'Book 33', 'Publisher 15', 'Third Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (34, 12, 'Book 34', 'Publisher 12', 'Fourth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (35, 17, 'Book 35', 'Publisher 17', 'Fifth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (36, 16, 'Book 36', 'Publisher 16', 'Sixth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (37, 20, 'Book 37', 'Publisher 20', 'Seventh Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (38, 14, 'Book 38', 'Publisher 14', 'Eighth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (39, 19, 'Book 39', 'Publisher 19', 'Ninth Edition');
-
-INSERT INTO Book (book_id, category_id, book_name, publisher, edition)
-VALUES (40, 18, 'Book 40', 'Publisher 18', 'Tenth Edition');
-
-
-
-
-
-
-INSERT INTO Reader (Id, name, phone_num, email, book_id)
-VALUES (1, 'Abdul Rahman', '01712345678', 'abdul@example.com', 1);
-
-INSERT INTO Reader (Id, name, phone_num, email, book_id)
-VALUES (2, 'Fatima Begum', '01823456789', 'fatima@example.com', 2);
-
-INSERT INTO Reader (Id, name, phone_num, email, book_id)
-VALUES (3, 'Kamal Hussain', '01934567890', 'kamal@example.com', 3);
-
-INSERT INTO Reader (Id, name, phone_num, email, book_id)
-VALUES (4, 'Nazma Akter', '01645678901', 'nazma@example.com', 4);
-
-INSERT INTO Reader (Id, name, phone_num, email, book_id)
-VALUES (5, 'Rahim Hossain', '01756789012', 'rahim@example.com', 5);
-
-INSERT INTO Reader (Id, name, phone_num, email, book_id)
-VALUES (6, 'Sultana Begum', '01867890123', 'sultana@example.com', 6);
-
-INSERT INTO Reader (Id, name, phone_num, email, book_id)
-VALUES (7, 'Mohammad Ali', '01978901234', 'mohammad@example.com', 7);
-
-INSERT INTO Reader (Id, name, phone_num, email, book_id)
-VALUES (8, 'Ayesha Khan', '01689012345', 'ayesha@example.com', 8);
-
-INSERT INTO Reader (Id, name, phone_num, email, book_id)
-VALUES (9, 'Rahima Khatun', '01790123456', 'rahima@example.com', 9);
-
-INSERT INTO Reader (Id, name, phone_num, email, book_id)
-VALUES (10, 'Mustafa Rahman', '01801234567', 'mustafa@example.com', 10);
-
-
-
-
-INSERT INTO Order_book (Issue_no, Book_id, Reader_id, issue_date) VALUES (1, 1, 1, to_date('2023-05-01','yyyy-mm-dd'));
-INSERT INTO Order_book (Issue_no, Book_id, Reader_id, issue_date) VALUES (2, 2, 1, to_date('2023-05-02','yyyy-mm-dd'));
-INSERT INTO Order_book (Issue_no, Book_id, Reader_id, issue_date) VALUES (3, 3, 3, to_date('2023-05-03','yyyy-mm-dd'));
-INSERT INTO Order_book (Issue_no, Book_id, Reader_id, issue_date) VALUES (4, 4, 4, to_date('2023-05-04','yyyy-mm-dd'));
-INSERT INTO Order_book (Issue_no, Book_id, Reader_id, issue_date) VALUES (5, 1, 2, to_date('2023-05-05','yyyy-mm-dd'));
-INSERT INTO Order_book (Issue_no, Book_id, Reader_id, issue_date) VALUES (6, 4, 2, to_date('2023-05-06','yyyy-mm-dd'));
-INSERT INTO Order_book (Issue_no, Book_id, Reader_id, issue_date) VALUES (7, 5, 3, to_date('2023-05-07','yyyy-mm-dd'));
-INSERT INTO Order_book (Issue_no, Book_id, Reader_id, issue_date) VALUES (8, 8, 7, to_date('2023-05-08','yyyy-mm-dd'));
-INSERT INTO Order_book (Issue_no, Book_id, Reader_id, issue_date) VALUES (9, 1, 9, to_date('2023-05-09','yyyy-mm-dd'));
-INSERT INTO Order_book (Issue_no, Book_id, Reader_id, issue_date) VALUES (10, 3, 10, to_date('2023-05-10','yyyy-mm-dd'));
-
-
-
---Displaying table data using SELECT command
-select * from library;
-select * from library where self_no=2;
-
---update value
-update library set self_no=1 where row_no=2;
-select * from book;
-
---all from book where book are in self 1
-select * from book where category_id in (select category_id from library where self_no=1);
-
---union,intersection,expect
-
---list of category which are in self 1 or row 2
-(select category from library where self_no=1) union (select category from library where row_no=2);
-
---list of category which are in self 1 and row 2
-(select category from library where self_no=1) intersect (select category from library where row_no=2);
-
-
---list of category which are in self 1 but no in row 2
-(select category from library where self_no=1) minus (select category from library where row_no=2);
-
-
---Aggregate Function
-
---count all entites from any table
-select count(category_id) from book where category_id=1;
-
---show how many book are under any category
-select category_id,count(category_id) as quantity from book group by category_id order by category_id;
-
---show how many book are under any category greater then 1
- select category_id,count(category_id) as quantity from book group by category_id having count(category_id)>1 order by category_id;
-
---show the user who order fiction category book
-select id,name from reader where id in 
-( select reader_id from order_book where book_id in
- ( select book_id from book where category_id =
- (select category_id from library where category='Fiction')));
-
---string that contain a
-select * from library where category like '%a%';
-
--- join
-select * from library natural join book where library.category='Fiction';
-
-select * from library join book using(category_id);
-
-select * from library left outer join book using(category_id);
-
-select * from library right outer join book using(category_id);
-
-select * from library full outer join book using(category_id);
-
-
+CREATE TABLE Library (
+  category_id INT PRIMARY KEY,
+  category VARCHAR(30),
+  self_no INT CHECK (self_no BETWEEN 1 AND 10),
+  row_no INT CHECK (row_no BETWEEN 1 AND 5)
+);
+
+CREATE TABLE Book (
+  book_id INT PRIMARY KEY,
+  category_id INT,
+  book_name VARCHAR(30),
+  publisher VARCHAR(30),
+  edition VARCHAR(50),
+  FOREIGN KEY (category_id) REFERENCES Library(category_id)
+);
+
+
+CREATE TABLE Reader (
+  Id INT PRIMARY KEY,
+  name VARCHAR(30),
+  phone_num NUMERIC(11),
+  email VARCHAR(30),
+  book_id INT,
+  FOREIGN KEY (book_id) REFERENCES Book(book_id)
+);
+
+CREATE TABLE Order_book (
+  Issue_no INT PRIMARY KEY,
+  Book_id INT,
+  Reader_id INT,
+  issue_date DATE,
+  FOREIGN KEY (Book_id) REFERENCES Book(book_id),
+  FOREIGN KEY (Reader_id) REFERENCES Reader(Id)
+);
+
+select table_name from user_tables; //show all table
+
+//add column
+alter table book add(puslish_year char(4));
+desc book;
+
+//modify domain 
+alter table book modify (publish_year numeric(4));
+desc book;
+
+//rename column name
+alter table book rename column category_id to cat_id;
+desc book;
+alter table book rename column cat_id to category_id;
+
+//delete column
+alter table book drop column publish_year;
+desc column;
+
+//rename table name
+rename book to book_info;
+select table_name from user_tables;
+rename book_info to book;
